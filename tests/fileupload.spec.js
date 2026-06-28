e8695eac31697c3b655554adb435f1e9a25d6897
SureshDeepthi19 <sureshsurya1126@gmail.com>
2026-06-28 23:10:00 +0530

    updated

--- tests/fileupload.spec.js
+++ tests/fileupload.spec.js
@@ -1,9 +1,10 @@
  // How To upload files
  const { test, expect } = require("@playwright/test");
 
- test ('Handle upload files', async ({page})=> {
+test ('Handle upload files', async ({page})=> {
    await page.goto("https://demoqa.com/upload-download")
    await page.locator("#uploadFile").setInputFiles("upload_files/tiger.jpg")
+
 
    
 
