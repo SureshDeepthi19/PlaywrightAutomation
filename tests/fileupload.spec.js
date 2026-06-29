c35723b62d8e37a98829278dba179cf36965674b
SureshDeepthi19 <sureshsurya1126@gmail.com>
2026-06-29 16:10:50 +0530

    add modified

--- tests/fileupload.spec.js
+++ tests/fileupload.spec.js
@@ -3,9 +3,13 @@
 
 test ('Handle upload files', async ({page})=> {
    await page.goto("https://demoqa.com/upload-download")
-   await page.locator("#uploadFile").setInputFiles("upload_files/tiger.jpg")
+   page.locator("#uploadFile").setInputFiles("upload_files/tiger.jpg")
 
-
+   const [download] = await Promise.all([
+   page.waitForEvent("download"),
+   page.locator("#downloadButton").click()
    
+ ])
+
 
 })
